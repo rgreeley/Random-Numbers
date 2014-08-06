bb2c28c75ddd6b50d43b32d17e6f3958e693dbf0
Robert Greeley <rgreeley@zappos.com>
2014-08-05 23:11:25 -0700

    Dice EX45

---
+++ Dice.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Dice
+{
+		public static void main( String[] args )
+		{
+		
+			Random r = new Random();
+
+			System.out.println( "Press ENTER to see the roll of the dice!" );
+			System.out.println();
+
+			int dice1 = 1 + r.nextInt(6), dice2 = 1 + r.nextInt(6);
+
+			System.out.println( "Roll #1: " + dice1 );
+			System.out.println( "Roll #2: " + dice2 );
+			System.out.println( "The total is " + (dice1 + dice2) +"!" );
+		}
+}
+
+
+
+
